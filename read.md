39d59f7b63fea8b84b1d29e9be5a58ce68ccac15
Gautam Tamang <gautam.tamang0000@gmail.com>
2022-01-08 09:16:33 +0545

    Using ModelSerializer

--- read.md
+++ read.md
@@ -213,3 +213,20 @@ def bookDetailView(request, pk):
             return Response(serializer.errors)
 
 ```
+
+## Using ModelSerializer
+> serializers.py
+```python
+from django.db.models import fields
+from app.models import Book
+from rest_framework import serializers
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+
+        # fields = ['tittle', 'author']     Only selected fields data will be shown
+        # Whicf field you wan to serialize
+        fields = '__all__'
+```
--- using_decorators/serializers.py
+++ using_decorators/serializers.py
@@ -1,18 +1,12 @@
+from django.db.models import fields
 from app.models import Book
 from rest_framework import serializers
 
 
-class BookSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=80)
-    author = serializers.CharField(max_length=80)
-    price = serializers.IntegerField(default=0)
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
 
-    def create(self, validated_data):  # this validated_data comes in dictionary form
-        return Book.objects.create(**validated_data)
-
-
-    def update(self, instance, validated_data):
-        newBook = Book(**validated_data)
-        newBook.id = instance.id
-        newBook.save()
-        return newBook
+        # fields = ['tittle', 'author']     Only selected fields data will be shown
+        # Whicf field you wan to serialize
+        fields = '__all__'
